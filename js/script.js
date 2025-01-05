88bcdc3f4e03bdb275699c966af8d61c3b89104c
Amaral Neto <alunoamaralneto@gmail.com>
2025-01-05 15:47:06 -0300

    Criado site oficial da sejuve de amontada

---
+++ js/script.js
@@ -0,0 +1,44 @@
+// Muda a cor da navbar quando estiver sobre a div "booking" //
+window.addEventListener('scroll', alterarCorNavbar)
+
+
+function alterarCorNavbar() {
+    const navbar = document.querySelector('nav')
+    const booking = document.querySelector('.booking')
+    const bookingRect = booking.getBoundingClientRect();
+
+    if (bookingRect.top < 50) {
+        navbar.classList.add('navbar-solida');
+    } else {
+        navbar.classList.remove('navbar-solida');
+    }
+}
+
+// CARROSSEL //
+const funcionarios = document.querySelectorAll('.funcionario');
+let funcionarioAtual = 0;
+function mostrarSlide(index) {
+    funcionarios.forEach((funcionario, i) => {
+        if (i === index) {
+            funcionario.style.display = 'block';
+        } else {
+            funcionario.style.display = 'none';
+        }
+    })
+}
+
+function proximo() {
+    funcionarioAtual = (funcionarioAtual + 1) % funcionarios.length;
+    mostrarSlide(funcionarioAtual)
+}
+
+function anterior() {
+    funcionarioAtual = (funcionarioAtual - 1 + funcionarios.length) % funcionarios.length;
+    mostrarSlide(funcionarioAtual);
+}
+
+document.querySelector('.anterior').addEventListener('click', anterior);
+document.querySelector('.proximo').addEventListener('click', proximo);
+
+// Mostrar o primeiro funcionário ao carregar a página //
+mostrarSlide(funcionarioAtual)
